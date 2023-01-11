93b7e688afefb26953c5b2c255edc36325d67a4d
Kenny Chu <kennychu@asl.com.hk>
2023-01-11 10:15:15 +0800

    added file upload

--- src/main.ts
+++ src/main.ts
@@ -8,27 +8,28 @@ function selectElement(selector: string) {
 const originalImg = selectElement('#img-original') as HTMLImageElement;
 const afterImgContainer = selectElement('#after-container') as HTMLDivElement;
 const SELECTED_COLOR = selectElement('#color-value') as HTMLInputElement;
-const PRIMARY_COLOR_DISPLAY = selectElement('#main-color input') as HTMLInputElement
-const SECONDARY_COLOR_DISPLAY = selectElement('#secondary-color input') as HTMLInputElement
-const TERTIARY_COLOR_DISPLAY = selectElement('#tertiary-color input') as HTMLInputElement
-const REMOVE_COLOR_BUTTON = selectElement('#remove-selected-color-btn') as HTMLButtonElement
+const PRIMARY_COLOR_DISPLAY = selectElement('#main-color input') as HTMLInputElement;
+const SECONDARY_COLOR_DISPLAY = selectElement('#secondary-color input') as HTMLInputElement;
+const TERTIARY_COLOR_DISPLAY = selectElement('#tertiary-color input') as HTMLInputElement;
+const REMOVE_COLOR_BUTTON = selectElement('#remove-selected-color-btn') as HTMLButtonElement;
+const UPLOAD_IMAGE_INPUT = selectElement('#image-upload') as HTMLInputElement;
 
-
-const width = originalImg.naturalWidth;
-const height = originalImg.naturalHeight;
+let WIDTH = originalImg.naturalWidth;
+let HEIGHT = originalImg.naturalHeight;
 
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d')!;
-canvas.width = width;
-canvas.height = height;
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
 
-ctx.drawImage(originalImg, 0, 0, width, height);
+ctx.drawImage(originalImg, 0, 0, WIDTH, HEIGHT);
 
-const imageData = ctx.getImageData(0, 0, width, height);
-const pixelData = imageData.data;
+let IMAGE_DATA = ctx.getImageData(0, 0, WIDTH, HEIGHT);
+let PIXEL_DATA = IMAGE_DATA.data;
+
 let count: any = {};
-for (let i = 0; i < pixelData.length; i += 4) {
-  const rgba = [pixelData[i], pixelData[i + 1], pixelData[i + 2], pixelData[i + 3]];
+for (let i = 0; i < PIXEL_DATA.length; i += 4) {
+  const rgba = [PIXEL_DATA[i], PIXEL_DATA[i + 1], PIXEL_DATA[i + 2], PIXEL_DATA[i + 3]];
   let stringRGBA = JSON.stringify(rgba);
   if (!count[stringRGBA]) {
     count[stringRGBA] = 1;
@@ -37,41 +38,41 @@ for (let i = 0; i < pixelData.length; i += 4) {
   }
 }
 
-let colorValuesInOrder = Object.entries(count) as any 
+let colorValuesInOrder = Object.entries(count) as any;
 colorValuesInOrder = colorValuesInOrder.sort((a: [string, number], b: [string, number]) => {
   if (a[1] < b[1]) {
-    return 1
+    return 1;
   } else {
-    return -1
+    return -1;
   }
-})
+});
 
 const TOP_3_COLORS = colorValuesInOrder.slice(0, 3);
 
-console.log(JSON.parse(TOP_3_COLORS[0][0]))
+console.log(JSON.parse(TOP_3_COLORS[0][0]));
 
-const MAIN_COLOR_RGBA = JSON.parse(TOP_3_COLORS[0][0])
-const SECONDARY_COLOR_RGBA = JSON.parse(TOP_3_COLORS[1][0])
-const TERTIARY_COLOR_RGBA = JSON.parse(TOP_3_COLORS[2][0])
+const MAIN_COLOR_RGBA = JSON.parse(TOP_3_COLORS[0][0]);
+const SECONDARY_COLOR_RGBA = JSON.parse(TOP_3_COLORS[1][0]);
+const TERTIARY_COLOR_RGBA = JSON.parse(TOP_3_COLORS[2][0]);
 
 const MAIN_COLOR_RGB = {
   r: MAIN_COLOR_RGBA[0],
   g: MAIN_COLOR_RGBA[1],
   b: MAIN_COLOR_RGBA[2],
-}
+};
 const SECONDARY_COLOR_RGB = {
   r: SECONDARY_COLOR_RGBA[0],
   g: SECONDARY_COLOR_RGBA[1],
   b: SECONDARY_COLOR_RGBA[2],
-}
+};
 const TERTIARY_COLOR_RGB = {
   r: TERTIARY_COLOR_RGBA[0],
   g: TERTIARY_COLOR_RGBA[1],
   b: TERTIARY_COLOR_RGBA[2],
-}
-PRIMARY_COLOR_DISPLAY.value = RGBToHex(MAIN_COLOR_RGB)
-SECONDARY_COLOR_DISPLAY.value = RGBToHex(SECONDARY_COLOR_RGB)
-TERTIARY_COLOR_DISPLAY.value = RGBToHex(TERTIARY_COLOR_RGB)
+};
+PRIMARY_COLOR_DISPLAY.value = RGBToHex(MAIN_COLOR_RGB);
+SECONDARY_COLOR_DISPLAY.value = RGBToHex(SECONDARY_COLOR_RGB);
+TERTIARY_COLOR_DISPLAY.value = RGBToHex(TERTIARY_COLOR_RGB);
 
 // for (let i = 0; i < pixelData.length; i+=4) {
 //   const rgba = [pixelData[i], pixelData[i+1], pixelData[i+2], pixelData[i+3]];
@@ -81,7 +82,7 @@ TERTIARY_COLOR_DISPLAY.value = RGBToHex(TERTIARY_COLOR_RGB)
 //   }
 // }
 
-ctx.putImageData(imageData, 0, 0);
+ctx.putImageData(IMAGE_DATA, 0, 0);
 
 afterImgContainer.prepend(canvas);
 
@@ -94,16 +95,16 @@ originalImg.addEventListener('click', (e) => {
   let imageX = Math.round(x - imgPos.x);
   const imageY = Math.round(y - imgPos.y);
 
-  const pixelIndex = imageY * 4 * width + imageX * 4;
+  const pixelIndex = imageY * 4 * WIDTH + imageX * 4;
 
   const pixel = {
-    r: pixelData[pixelIndex],
-    g: pixelData[pixelIndex + 1],
-    b: pixelData[pixelIndex + 2],
-    a: pixelData[pixelIndex + 3],
+    r: PIXEL_DATA[pixelIndex],
+    g: PIXEL_DATA[pixelIndex + 1],
+    b: PIXEL_DATA[pixelIndex + 2],
+    a: PIXEL_DATA[pixelIndex + 3],
   };
 
-  SELECTED_COLOR.dataset.rgba = JSON.stringify([pixelData[pixelIndex], pixelData[pixelIndex+1], pixelData[pixelIndex+2], pixelData[pixelIndex+3]]);
+  SELECTED_COLOR.dataset.rgba = JSON.stringify([PIXEL_DATA[pixelIndex], PIXEL_DATA[pixelIndex + 1], PIXEL_DATA[pixelIndex + 2], PIXEL_DATA[pixelIndex + 3]]);
   SELECTED_COLOR.value = RGBToHex(pixel);
 });
 
@@ -121,13 +122,36 @@ function RGBToHex(pixel: { r: number; g: number; b: number }) {
 }
 
 REMOVE_COLOR_BUTTON.addEventListener('click', () => {
-  const RGBA = SELECTED_COLOR.dataset.rgba
-  for (let i = 0; i < pixelData.length; i += 4) {
-    const rgba = [pixelData[i], pixelData[i + 1], pixelData[i + 2], pixelData[i + 3]];
+  const RGBA = SELECTED_COLOR.dataset.rgba;
+  for (let i = 0; i < PIXEL_DATA.length; i += 4) {
+    const rgba = [PIXEL_DATA[i], PIXEL_DATA[i + 1], PIXEL_DATA[i + 2], PIXEL_DATA[i + 3]];
     let stringRGBA = JSON.stringify(rgba);
     if (stringRGBA === RGBA) {
-      pixelData[i+3] = 0
+      PIXEL_DATA[i + 3] = 0;
     }
   }
-  ctx.putImageData(imageData, 0, 0);
-})
+  ctx.putImageData(IMAGE_DATA, 0, 0);
+});
+
+UPLOAD_IMAGE_INPUT.addEventListener('change', (e: any) => {
+  const url = URL.createObjectURL(e.target.files[0]);
+  const image = new Image();
+  image.src = url;
+  image.onload = () => {
+    // originalImg.width = image.naturalWidth;
+    // originalImg.height = image.naturalHeight;
+    originalImg.src = url;
+    WIDTH = originalImg.width;
+    HEIGHT = originalImg.height;
+    renderAfterImage(ctx, image);
+  };
+});
+
+function renderAfterImage(ctx: CanvasRenderingContext2D, image: HTMLImageElement) {
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
+
+  ctx.drawImage(image, 0, 0, WIDTH, HEIGHT);
+  IMAGE_DATA = ctx.getImageData(0, 0, WIDTH, HEIGHT);
+  PIXEL_DATA = IMAGE_DATA.data;
+}
